393baf7b2accc11b8bc0f01cd2d9cfe3f9bf1c17
Helaine <helaine.alves.pontes07@aluno.ifce.edu.br>
2020-08-14 09:32:39 -0300

    Initial Commit

---
+++ NanoIndividual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NanoIndividual
+//
+//  Created by Helaine Pontes on 14/08/20.
+//  Copyright © 2020 Helaine Pontes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NanoIndividualTests/NanoIndividualTests.swift
@@ -0,0 +1,34 @@
+//
+//  NanoIndividualTests.swift
+//  NanoIndividualTests
+//
+//  Created by Helaine Pontes on 14/08/20.
+//  Copyright © 2020 Helaine Pontes. All rights reserved.
+//
+
+import XCTest
+@testable import NanoIndividual
+
+class NanoIndividualTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
